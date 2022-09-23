f7f34bf785b4e28ac7254449c86d8eeff18087d5
Priya Goli <ratnapriya.goli@gmail.com>
2022-09-23 12:46:58 +0100

    test commit

--- 2itest.js
+++
@@ -1,8 +0,0 @@
-function getRandomnum(min, max) {
-    min = Math.ceil(min);
-    max= Math.floor(max);
-    return Math.floor(Math.random() * (1+ max - min)) + min
-}
-console.log(getRandomnum(1,100));
-
-
